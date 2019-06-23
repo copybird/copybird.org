40a1d696bb3c6f1526e0730b9bc4d830ab1499da
denisoed <denisoed@gmail.com>
2019-06-23 20:11:11 +0600

    Added slider for database section

--- app/dev/js/scripts.js
+++ app/dev/js/scripts.js
@@ -1,6 +1,10 @@
 //jQuery is required to run this code
-// $( document ).ready(function() {
-
-//     // Custom Code
-
-// });
+$(document).ready(function () {
+    $('#database-slider').slick({
+        slidesToShow: 3,
+        autoplay: true,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+        nextArrow: '<button type="button" class="slick-prev">Previous</button>'
+    });
+});
