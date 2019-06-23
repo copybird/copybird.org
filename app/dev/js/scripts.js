0d5c87f493c90a743599794f93d719c346f87f05
denisoed <denisoed@gmail.com>
2019-06-23 21:28:13 +0600

    Updated logo | added slider for notifications section

--- app/dev/js/scripts.js
+++ app/dev/js/scripts.js
@@ -7,4 +7,12 @@ $(document).ready(function () {
         prevArrow: '<button type="button" class="slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-next"></button>'
     });
+
+    $('#notification-slider').slick({
+        slidesToShow: 3,
+        autoplay: true,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>'
+    });
 });
